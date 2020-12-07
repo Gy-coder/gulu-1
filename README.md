d52ecc484835878a6dd8a6d80ced8061636d1aa4
Gy <406631892@qq.com>
2020-12-07 11:53:20 +0800

    完成按钮基本样式

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 咕噜 一个Vue轮子
+
+使用本框架前需要请在css中使用border-box
+
+```css
+*{box-sizing: border-box;}
+```
---
+++ button.js
@@ -0,0 +1,6 @@
+Vue.component('g-button',{
+  el:'#app',
+  template:`
+    <button class="g-button">hi</button>
+  `
+})
