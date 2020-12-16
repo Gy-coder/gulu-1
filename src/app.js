17319a9c91f370eb1004600a5ef32e0304b24d27
Gy <406631892@qq.com>
2020-12-16 16:41:20 +0800

    添加input.vue以及其样式 修改app.js中的g-button

--- src/app.js
+++ src/app.js
@@ -2,34 +2,40 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-input',Input)
+
 
 new Vue({
   el: '#app',
   template: `
   <div id="app">
-    <g-button @click="x" :loading="loading1">按钮</g-button>
-    <g-button icon="settings" :loading="loading1" icon-position="right">按钮</g-button>
-    <g-button icon="settings" :loading="loading1">按钮</g-button>
-    <g-button-group>
-      <g-button icon="left">上一页</g-button>
-      <g-button>更多</g-button>
-      <g-button icon="right">下一页</g-button>
-    </g-button-group>
-    <div id="test"></div>
+    <div class="box">
+      <g-button>按钮</g-button>
+      <g-button :loading="loading1" @click="loading1 = !loading1">按钮</g-button>
+      <g-button icon="settings"  icon-position="right">按钮</g-button>
+      <g-button icon="settings" >按钮</g-button>
+      <g-button-group>
+        <g-button icon="left">上一页</g-button>
+        <g-button>更多</g-button>
+        <g-button icon="right">下一页</g-button>
+      </g-button-group>
+    </div>
+    <div class="box">
+      <g-input value="g-input"></g-input>
+      <g-input value="g-input" disabled></g-input>
+      <g-input value="g-input" readonly></g-input>
+      <g-input value="g-input" error="密码错误"></g-input>
+    </div>
    </div>
   `,
   data(){
     return {
       loading1: false
-    }
-  },
-  methods:{
-    x(){
-      this.loading1 = !this.loading1
     }
   }
 })
@@ -38,73 +44,3 @@ new Vue({
 // 单元测试
 
 
-import chai from 'chai'
-import spies from 'chai-spies'
-
-
-const  expect = chai.expect;
-chai.use(spies)
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings'
-    }
-  })
-  vm.$mount('#test')
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-settings')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'settings',
-      loading:true
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPosition:'right'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-    }
-  })
-  vm.$mount()
-  let spy = chai.spy(function(){
-
-  })
-  vm.$on('click',spy)
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
-}
